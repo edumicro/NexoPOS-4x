13a396620033f5bd6ec332440e2d200666abcd92
Eduardo <eduardo@microvalencia.es>
2022-05-02 13:42:38 +0200

    trans

--- resources/views/pages/auth/sign-in.blade.php
+++ resources/views/pages/auth/sign-in.blade.php
@@ -11,9 +11,9 @@ use App\Classes\Output;
             <div id="sign-in-box" class="w-full md:w-3/5 lg:w-2/5 xl:w-84">
                 <div class="flex justify-center items-center py-6">
                     @if ( ! ns()->option->get( 'ns_store_square_logo', false ) )
-                    <img class="w-32" src="{{ asset( 'svg/nexopos-variant-1.svg' ) }}" alt="NexoPOS">
+                    <img class="w-32" src="{{ asset( 'svg/nexopos-variant-1.svg' ) }}" alt="NIUTPV">
                     @else
-                    <img src="{{ ns()->option->get( 'ns_store_square_logo' ) }}" alt="NexoPOS">
+                    <img src="{{ ns()->option->get( 'ns_store_square_logo' ) }}" alt="NIUTPV">
                     @endif
                 </div>
                 <x-session-message></x-session-message>
